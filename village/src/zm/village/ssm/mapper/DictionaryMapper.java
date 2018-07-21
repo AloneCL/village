58915cbfd7116754ce43b2f6bea7aaae294f6c08
ALONGER_C <along9271_c@qq.com>
2018-07-21 16:57:32 +0800

    mapper修改 管理员 收藏 数据字典 标签 标签字典

--- village/src/zm/village/ssm/mapper/DictionaryMapper.java
+++ village/src/zm/village/ssm/mapper/DictionaryMapper.java
@@ -2,7 +2,14 @@ package zm.village.ssm.mapper;
 
 import zm.village.dao.Dictionary;
 
-public interface DirInfMapper {
+/**
+* @ClassName: DictionaryMapper.java
+* @Description: 数据字典类数据库映射接口
+* @version: v1.0.0
+* @author: 陈光磊
+* @date: 2018年7月21日 上午11:46:46 
+ */
+public interface DictionaryMapper {
     int deleteByPrimaryKey(Integer dirId);
 
     int insert(Dictionary record);

a99e67867d3327ea0f9de7fdd4b21cb2950b9233
Lizifan <695199262@qq.com>
2018-10-14 19:04:30 +0800

    验证码BUG修复

--- village/src/zm/village/ssm/webcontroller/CheckController.java
+++ village/src/zm/village/ssm/webcontroller/CheckController.java
@@ -19,13 +19,12 @@ public class CheckController{
 		String result = "error";
         String clientCheckcode = request.getParameter("validateCode");//接收客户端浏览器提交上来的验证码
         String serverCheckcode = (String) request.getSession().getAttribute("checkcode");//从服务器端的session中取出验证码
-        if (clientCheckcode.equals(serverCheckcode.toLowerCase())) {//将客户端验证码和服务器端验证比较，如果相等，则表示验证通过
+        System.out.println(serverCheckcode);
+        if (clientCheckcode.toLowerCase().equals(serverCheckcode.toLowerCase())) {//将客户端验证码和服务器端验证比较，如果相等，则表示验证通过
         	System.out.println("验证码验证通过！");
         	result = "ok";
         	response.getWriter().print(result);
-        }
-        else
-        {
+        } else {
             System.out.println("验证码验证失败！");
             response.getWriter().print(result);
         }
